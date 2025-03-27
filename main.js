3feaaa8d28d90744830d6df7d9ebeb534fde1a79
Enrique Vidó Villalvazo <enriqu_vido23@outlook.com>
2025-03-26 23:43:43 -0600

    electron

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadFile('index.html');
+  }
+});

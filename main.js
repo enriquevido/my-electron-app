bdc2dfe703d8f29616dcf912f87033c7b9962fd4
Enrique Vidó Villalvazo <enriqu_vido23@outlook.com>
2025-03-27 07:56:39 -0600

    add renderer

--- main.js
+++ main.js
@@ -1,32 +1,26 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron/main')
 
-let mainWindow;
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
 
 app.whenReady().then(() => {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+  createWindow()
 
-  mainWindow.loadFile('index.html');
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-});
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
-    mainWindow.loadFile('index.html');
-  }
-});
+})
